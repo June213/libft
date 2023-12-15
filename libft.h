312ddee52edef7ae2d44b77eb8dcb2e87c51e01d
jsalaber <jsalaber@student.42urduliz.com>
2023-12-15 13:15:18 +0100

    tolower y toupper

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jsalaber <jsalaber@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 11:58:59 by jsalaber          #+#    #+#             */
-/*   Updated: 2023/12/15 11:01:02 by jsalaber         ###   ########.fr       */
+/*   Updated: 2023/12/15 13:13:12 by jsalaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
--- makefile
+++ makefile
@@ -6,14 +6,14 @@
 #    By: jsalaber <jsalaber@student.42urduliz.co    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/12/13 15:19:15 by jsalaber          #+#    #+#              #
-#    Updated: 2023/12/15 12:15:44 by jsalaber         ###   ########.fr        #
+#    Updated: 2023/12/15 13:13:24 by jsalaber         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME    = libft.a
 SRCS    = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c \
 		ft_strlen.c ft_memset.c ft_bzero.c ft_memcpy.c ft_memmove.c ft_strlcpy.c \
-		ft_strlcat.c \
+		ft_strlcat.c ft_toupper.c ft_tolower.c \
 		
 OBJS    = $(SRCS:%.c=%.o)
 CFLAGS  = -Wall -Wextra -Werror
