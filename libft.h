75d0543660a6ca9f578a6f6ed077e6f7ce166f20
jsalaber <jsalaber@student.42urduliz.com>
2023-12-26 15:15:57 +0100

    strdup

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jsalaber <jsalaber@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 11:58:59 by jsalaber          #+#    #+#             */
-/*   Updated: 2023/12/26 14:57:13 by jsalaber         ###   ########.fr       */
+/*   Updated: 2023/12/26 15:08:13 by jsalaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s);
 
 #endif
