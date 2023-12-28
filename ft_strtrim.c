028910240d84984db6f49ae3a75167e8c95df66e
jsalaber <jsalaber@student.42urduliz.com>
2023-12-28 10:08:43 +0100

    strtrim corregido

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: junesalaberria <junesalaberria@student.    +#+  +:+       +#+        */
+/*   By: jsalaber <jsalaber@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 17:19:36 by junesalaber       #+#    #+#             */
-/*   Updated: 2023/12/27 18:12:28 by junesalaber      ###   ########.fr       */
+/*   Updated: 2023/12/28 09:42:31 by jsalaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,23 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char	*str;
 	size_t	start;
 	size_t	len;
+	size_t	new_len;
 
 	if (!s1 || !set)
 		return (NULL);
 	start = 0;
 	len = ft_strlen(s1);
 	while (s1[start] != '\0' && ft_strchr(set, s1[start]))
-	{
 		start++;
-	}
-	while (ft_strchr(set, s1[len - 1]) && len > start)
-	{
+	if (start == len)
+		return (ft_strdup(""));
+	while (s1[len - 1] && ft_strchr(set, s1[len - 1]) && len > start)
 		len--;
-	}
-	str = (char *)malloc((len - start + 1) * sizeof(char));
+	new_len = len - start;
+	str = (char *)malloc(sizeof(char) * (new_len + 1));
 	if (!str)
 		return (NULL);
-	ft_strlcpy(str, &s1[start], (len - start + 1));
+	ft_strlcpy(str, &s1[start], (new_len + 1));
 	str[len - start] = '\0';
 	return (str);
 }
