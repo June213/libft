c5707f9537d1d376e0bc2b2c5a3e7340255b97d6
June Salaberria <junesalaberria@MacBook-Air-de-June.local>
2024-01-02 11:44:45 +0100

    itoa

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: junesalaberria <junesalaberria@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 11:58:59 by jsalaber          #+#    #+#             */
-/*   Updated: 2023/12/27 17:37:58 by junesalaber      ###   ########.fr       */
+/*   Updated: 2024/01/02 10:49:13 by junesalaber      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,5 +45,7 @@ char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif
